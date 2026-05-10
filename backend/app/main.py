bca11d154c6c0e892d9e2562b6a8d49a4b5aff4a
DISHARI GHOSH <disharighosh77@gmail.com>
2026-05-10 19:41:21 +0530

    Update CORS origin for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="SumifyX API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                  "https://sumifyx.onrender.com"
+                  "https://sumifyx.vercel.app"
                   ],
     allow_credentials=True,
     allow_methods=["*"],
